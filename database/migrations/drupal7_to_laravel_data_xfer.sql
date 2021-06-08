1ed1368130864fa4d61f6bcdb09b54b2a24cb7e3
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-06-08 15:26:51 -0500

    initial data transfer script from Drupal 7 site to Laravel

---
+++ database/migrations/drupal7_to_laravel_data_xfer.sql
@@ -0,0 +1,7 @@
+INSERT INTO fuse_laravel.challenges (name, description, d7_id)
+SELECT name, description, tid as d7_id from fuse.taxonomy_term_data ttd WHERE ttd.vid = (
+SELECT fuse.taxonomy_vocabulary.vid
+FROM fuse.taxonomy_vocabulary taxonomy_vocabulary
+WHERE (taxonomy_vocabulary.machine_name = 'challenges')
+)
+ORDER BY name;
