49444babb617f9be7bc009ea63d9dd8f4666af00
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-08-13 14:54:57 -0500

    updates to challenge version table and addition of challenge category table (taxonomy)

--- database/migrations/2021_05_07_215224_create_challenge_versions_table.php
+++ database/migrations/2021_05_07_215224_create_challenge_versions_table.php
@@ -13,16 +13,58 @@ class CreateChallengeVersionsTable extends Migration
      */
     public function up()
     {
+        Schema::create('challenge_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->string('name');
+            $table->text('description');
+            // Migration Columns.
+            $table->unsignedBigInteger('d7_id');
+        });
+
         Schema::create('challenge_versions', function (Blueprint $table) {
             $table->id();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
             $table->foreignId('challenge_id')
                   ->constrained();
-            $table->string('name');
+            $table->foreignId('challenge_category_id')
+                  ->constrained();
+            $table->json('name');
+            $table->json('blurb')
+                  ->nullable();
+            $table->json('summary')
+                  ->nullable();
+            $table->json('stuff_you_need')
+                  ->nullable();
+            $table->json('chromebook_info')
+                  ->nullable();
+            $table->json('facilitator_notes')
+                  ->nullable();
+            // $table->string('gallery_image');
+            // $table->string('gallery_wistia_video');
+            $table->string('gallery_version_desc_short')
+                  ->nullable();
+            $table->string('gallery_version_desc_long')
+                  ->nullable();
+            $table->string('slug')
+                  ->unique()
+                  ->nullable();
+            $table->foreignId('prerequisite_challenge_version_id')
+                  ->nullable()
+                  ->constrained('challenge_versions')
+                  ->cascadeOnUpdate()
+                  ->nullOnDelete();
+            $table->string('info_article_url')
+                  ->nullable()
+                  ->comment('ZenDesk Article URL for facilitators');
             // Migration Columns.
             $table->unsignedBigInteger('d7_id');
             $table->unsignedBigInteger('d7_challenge_id');
+            $table->unsignedBigInteger('d7_challenge_category_id');
+            $table->unsignedBigInteger('d7_prereq_challenge_id')
+                  ->nullable();
         });
     }
 
--- database/migrations/drupal7_to_laravel_data_xfer.sql
+++ database/migrations/drupal7_to_laravel_data_xfer.sql
@@ -7,6 +7,15 @@ WHERE (taxonomy_vocabulary.machine_name = 'challenges')
 )
 ORDER BY name;
 
+-- Insert Challenge Category Taxonomy Terms
+INSERT INTO fuse_laravel.challenge_categories (name, description, d7_id)
+SELECT name, description, tid as d7_id from fuse.taxonomy_term_data ttd WHERE ttd.vid = (
+SELECT fuse.taxonomy_vocabulary.vid
+FROM fuse.taxonomy_vocabulary taxonomy_vocabulary
+WHERE (taxonomy_vocabulary.machine_name = 'challenge_categories')
+)
+ORDER BY name;
+
 -- Insert Packages
 INSERT INTO fuse_laravel.packages (created_at, updated_at, name, description, student_activity_tab_access, d7_id)
 SELECT FROM_UNIXTIME(n.created), FROM_UNIXTIME(n.changed), n.title, fdb.body_value, fdfsat.field_student_activity_tab_value, n.nid as d7_id
@@ -34,22 +43,53 @@ LEFT JOIN fuse_laravel.packages p ON pivot.d7_package_id = p.d7_id
 SET pivot.challenge_id = c.id, pivot.package_id = p.id;
 
 -- Insert Challenge Versions
-INSERT INTO fuse_laravel.challenge_versions (name, challenge_id, d7_id, d7_challenge_id, created_at, updated_at)
-SELECT n.title as name, 1 as challenge_id, n.nid as d7_id, ttd.tid as d7_challenge_id, FROM_UNIXTIME(n.created), FROM_UNIXTIME(n.changed)
+INSERT INTO fuse_laravel.challenge_versions (
+  name,
+  challenge_id, challenge_category_id,
+  blurb, summary,
+  d7_id, d7_challenge_id, d7_challenge_category_id, d7_prereq_challenge_id,
+  created_at, updated_at
+)
+SELECT
+  JSON_OBJECT("en", n.title), 1, 1,
+  JSON_OBJECTAGG(fdfb.`language` , fdfb.field_blurb_value),
+  JSON_OBJECTAGG(fdb.`language`, fdb.body_value),
+  n.nid, challenge_ttd.tid, challenge_category_ttd.tid, fdfpc.field_prerequisite_challenges_nid,
+  FROM_UNIXTIME(n.created), FROM_UNIXTIME(n.changed)
 FROM fuse.node AS n
+LEFT JOIN fuse.field_data_field_blurb fdfb ON
+  fdfb.entity_type = 'node'
+  AND fdfb.bundle = 'challenge'
+  AND fdfb.entity_id = n.nid
+LEFT JOIN fuse.field_data_body fdb ON
+  fdb.entity_type = 'node'
+  AND fdb.bundle = 'challenge'
+  AND fdb.entity_id = n.nid
 LEFT JOIN fuse.field_data_field_version_of AS fdfvo ON
   (fdfvo.entity_type = 'node'
   AND fdfvo.bundle = 'challenge'
   AND fdfvo.entity_id = n.nid)
-JOIN fuse.taxonomy_term_data ttd
-  ON ttd.tid = fdfvo.field_version_of_target_id
+JOIN fuse.taxonomy_term_data challenge_ttd
+  ON challenge_ttd.tid = fdfvo.field_version_of_target_id
+LEFT JOIN fuse.field_data_field_challenge_category AS fdfcc ON
+  (fdfcc.entity_type = 'node'
+  AND fdfcc.bundle = 'challenge'
+  AND fdfcc.entity_id = n.nid)
+JOIN fuse.taxonomy_term_data challenge_category_ttd
+  ON challenge_category_ttd.tid = fdfcc.field_challenge_category_tid
+LEFT JOIN fuse.field_data_field_prerequisite_challenges AS fdfpc ON
+  (fdfpc.entity_type = 'node'
+  AND fdfpc.bundle = 'challenge'
+  AND fdfpc.entity_id = n.nid)
 WHERE n.type = 'challenge'
+GROUP BY n.nid
 ORDER BY n.title ASC;
 
 -- Update foreign keys
 UPDATE fuse_laravel.challenge_versions cv
 LEFT JOIN fuse_laravel.challenges c ON cv.d7_challenge_id = c.d7_id
-SET cv.challenge_id = c.id;
+LEFT JOIN fuse_laravel.challenge_categories cc ON cv.d7_challenge_category_id = cc.d7_id
+SET cv.challenge_id = c.id, cv.challenge_category_id = cc.id;
 
 -- Insert Levels
 INSERT INTO fuse_laravel.levels (created_at, updated_at, challenge_version_id, d7_id, d7_challenge_version_id)
