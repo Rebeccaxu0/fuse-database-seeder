d6ead31c4338972b7b28ab04f121fe223a0e650d
Emma Brock Mansell <ebm7895@dhcp-10-102-2-118.wireless.northwestern.private>
2021-10-22 15:10:44 -0500

    Skin for login page and accompanying tailwind config edits

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,159 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            FUSE
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang='en-us'>
+    <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    
 
-        <x-jet-validation-errors class="mb-4" />
+    <!-- <link rel="stylesheet" href="/css/flickity.min.css">
+    <script src="/js/flickity.pkgd.min.js"></script> -->
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>FUSE Studio</title><meta name="generator" content="Hugo 0.88.0" /><link rel="stylesheet" href="https://public.fusestudio.net/css/styles.min.e69930b8750c8b152445020d3e03700283b09fe7e94f56bcaf4e0405a0673b6d.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <!-- <link rel="manifest" href="/site.webmanifest"> -->
+</head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+    <body class="bg-gradient-to-t from-fuse-dk-teal to-fuse-teal">
+        <header class="bg-fuse-dk-teal p-2 h-18">
+  
+        <div class="container relative p-0" style="min-height: 2rem;">
+    
+        <a href="/" class="absolute left-0 mt-3"><img src="/logo.png" alt="logo" class="w-20"></a>
+    
+        <style type="text/css">
+            @media (max-width: 2000px) {
+                input#nav-toggle:checked~label#show-button {
+                    display: none;
+                }
 
+                input#nav-toggle:checked~label#hide-button {
+                    display: flex;
+                }
+
+                input#nav-toggle:checked~#nav-menu {
+                    display: block;
+                    border-radius: 6px;
+                }
+                #nav-menu {margin-top:4rem;}
+            }
+        </style>
+
+    <nav class="flex text-xs mx-auto pl-20">
+
+        <input id="nav-toggle" type=checkbox class="hidden">
+        <label id="show-button" for="nav-toggle"
+            class="mt-3 ml-4 text-gray-300 hover:text-white">
+            <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu Open</title>
+                <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
+            </svg>
+        </label>
+        <label id="hide-button" for="nav-toggle"
+            class="ml-4 hidden text-gray-300 hover:text-white">
+            <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu Close</title>
+                <polygon points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
+                    transform="rotate(45 10 10)" />
+            </svg>
+        </label> 
+
+        <ul id="nav-menu"
+            class="hidden absolute items-center top-0 left-20 z-10 text-auto bg-fuse-dk-teal">
+            
+            <li class="lg:inline-block items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/how-fuse-works/" class="w-full h-full px-2 py-4">How Fuse Works</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/get-started/" class="w-full h-full px-2 py-4">Get Started</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/challenges/" class="w-full h-full px-2 py-4">Challenges</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/locations/" class="w-full h-full px-2 py-4">Our Network</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/research/" class="w-full h-full px-2 py-4">Research</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/about/" class="w-full h-full px-2 py-4">About</a>
+            </li>
+            
+            <li class="items-center rounded-sm mx-1 px-2 uppercase font-semibold text-white hover:bg-fuse-teal">
+                <a class="text-white hover:no-underline" href="/blog" class="w-full h-full px-2 py-4">Blog</a>
+            </li>
+        </ul>
+
+    </nav>
+  </div>
+
+</header><main class="container mx-auto flex-shrink flex-grow lg:px-4 py-16">
+<article>
+    <div class="mx-auto my-auto">
+<div class="min-w-screen min-h-screen">
+<div class="">
+    <div class="lg:flex mx-auto my-auto w-2/3 lg:w-2/3 bg-gradient-to-t from-fuse-teal-100 to-white rounded-lg shadow-lg border p-8 sm:px-12">
+    <div class="lg:flex-1">
+    <img class="min-h-8 max-h-36 md:max-h-48 lg:max-h-72 lg:pr-12 lg:pt-24 xl:pt-16" src="/fuse-logo.svg">
+    </div>
+    <div class="lg:flex-1">
+    <h2 class="text-display">Sign In</h2>
+    <div class="grid grid-cols-1 gap-6 max-w-full">
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
-                <x-jet-label for="name" value="{{ __('Username') }}" />
+                <x-jet-label for="name" value="{{ __('Username') }}" class="block text-md text-gray-700" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="name" name="name" :value="old('name')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-label for="password" value="{{ __('Password') }}" class="block text-md text-gray-700"/>
+                <x-jet-input id="password" class="block mt-1 w-full" type="name" name="password" required autocomplete="current-password"  required autofocus/>
             </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+            <div class="mt-8">
+                <x-jet-button class="max-w-xs btn block w-full">
+                    {{ __('Sign in') }}
                 </x-jet-button>
             </div>
         </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+      <img src="/btn_google_signin_light_normal_web.png">
+      <p class="text-fuse-dk-teal md:text-s"> sign up | forgot username or password
+    </div>
+    </div>
+    </div>
+</div>
+
+</article>
+
+</main>
+<footer class="align-bottom bg-fuse-teal-500 md:grid md:grid-cols-2 px-20 py-10 mt-12 max-w-screen">
+    <div class="md:px-4 lg:px-10">
+        <h3 class="text-white font-extrabold text-4xl pb-10 font-display">30,000 students</h3>
+        <p class="leading-loose font-semibold"><a href="/privacy">Privacy Policy</a><br>
+        Terms of Use<br>
+        info@fusestudio<br>
+        About</p>
+    </div>
+    <div class="md:px-4 lg:px-10">
+        <h3 class="text-white font-extrabold text-4xl pb-10 font-display">200 schools</h3>
+        <p><strong>Translation</strong></p>
+    </div>
+</footer></body>
+</html>
