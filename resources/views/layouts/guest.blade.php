3de4e7a08388b7dc7f36dbd802372480ed36775a
Emma Brock Mansell <ebm7895@dhcp-10-102-2-118.wireless.northwestern.private>
2021-10-29 12:39:24 -0500

    Cleaning up and componenting the login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,20 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <!-- scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>  
+        
+        <!-- fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <style type="text/css">
+                @media (max-width: 2000px) {
+                    input#nav-toggle:checked~label#show-button {
+                        display: none;
+                    }
+                    input#nav-toggle:checked~label#hide-button {
+                        display: flex;
+                    }
+                    input#nav-toggle:checked~#nav-menu {
+                        display: block;
+                        border-radius: 6px;
+                    }
+                    #nav-menu {margin-top:4rem;}
+                }
+            </style>
     </head>
-    <body>
-        <div class="font-sans text-blue-900 antialiased">
-            {{ $slot }}
-        </div>
+    <body class="bg-gradient-to-t from-fuse-dk-teal to-fuse-teal">
+    <div>
+        {{ $slot }}
+    </div>
     </body>
 </html>
