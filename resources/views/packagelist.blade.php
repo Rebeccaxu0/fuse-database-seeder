5e1af03e407c439000f4386cb08ee46eff2a5067
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-11-05 15:42:15 -0500

    wrap strings in translate function; simplify html

--- resources/views/packagelist.blade.php
+++ resources/views/packagelist.blade.php
@@ -1,35 +1,27 @@
 <x-admin-layout>
-<article>
-    <div class="mx-auto my-auto py-16">
-    <div class="min-w-screen min-h-screen">
-        <div>
-            <div class="lg:flex mx-auto my-auto w-2/3 lg:w-2/3 bg-gradient-to-t from-fuse-teal-100 to-white rounded-lg shadow-lg border p-8 sm:px-12">
-            <div class="lg:flex-1">
-            <h2 class="mt-6 text-fuse-dk-teal text-center text-2xl font-semibold font-display">Packages</h2>
-            <div class="overflow-x-auto mt-6">
-            <table class="table w-full table-zebra">
-                <tbody>
-                    @foreach($data as $item)
-                    <tr>
-                        <h3 class="mt-2 mb-2"> {{$item->name}} </h3>
-                        <label class="text-xs"> {{$item->description}} </label>
-                        @foreach ($item->challenges as $challenge)
-                            <details>
-                                <summary>{{$item->challenges()->count()}} challenges</summary>
-                                <ol>
-                                    <li><label class="text-xs text-fuse-teal"> {{$challenge->name}} </label></li>
-                                </ol>
-                            </details>
-                        @endforeach
-                    <label class="text-xs"> Used by {{$item->districts()->count()}} districts, {{$item->schools()->count()}} schools, and {{$item->studios()->count()}} studios</label>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </div>
-        </div>
+  <article class="mx-auto my-auto py-16 min-w-screen min-h-screen">
+    <div class="mx-auto my-auto w-2/3 lg:w-2/3 bg-gradient-to-t from-fuse-teal-100 to-white rounded-lg shadow-lg border p-8 sm:px-12">
+      <h2 class="mt-6 text-fuse-dk-teal text-center text-2xl font-semibold font-display">{{ __('Packages') }}</h2>
+      <div class="overflow-x-auto mt-6">
+        @foreach($data as $item)
+        <h3 class="mt-2 mb-2">{{ $item->name }}</h3>
+        <label class="text-xs">{{ $item->description }}</label>
+        <details>
+          <summary>{{ __(':count challenges', ['count' => $item->challenges()->count()]) }}</summary>
+          <ol>
+            @foreach ($item->challenges as $challenge)
+            <li><label class="text-xs text-fuse-teal">{{ $challenge->name }}</label></li>
+            @endforeach
+          </ol>
+        </details>
+        <label class="text-xs">{{ __('Used by :district_count districts, :school_count schools, and :studio_count studios',
+          [
+          'district_count' => $item->districts()->count(),
+          'school_count' => $item->schools()->count(),
+          'studio_count' => $item->studios()->count()
+          ]) }}</label>
+        @endforeach
+      </div>
     </div>
-    </div>
-</article>
+  </article>
 </x-admin-layout>
-
