35d4d0a53a4d46270b800c850ca81955a80a0af8
Emma Brock Mansell <ebm7895@dhcp-10-102-2-118.wireless.northwestern.private>
2021-11-10 15:55:46 -0600

    added buttons to admin/packages

--- resources/views/packagelist.blade.php
+++ resources/views/packagelist.blade.php
@@ -2,10 +2,15 @@
   <article class="mx-auto my-auto py-16 min-w-screen min-h-screen">
     <div class="mx-auto my-auto w-2/3 lg:w-2/3 bg-gradient-to-t from-fuse-teal-100 to-white rounded-lg shadow-lg border p-8 sm:px-12">
       <h2 class="mt-6 text-fuse-dk-teal text-center text-2xl font-semibold font-display">{{ __('Packages') }}</h2>
-      <a href="{{ route('login') }}" class="text-sm text-fuse-green">Add package</a> <!-- need to change link -->
+      <button href="{{ route('login') }}" class="text-md h-12 px-6 m-2 bg-fuse-green rounded-lg text-white">Add package</button> <!-- need to change link -->
       <div class="overflow-x-auto mt-6">
         @foreach($data as $item)
-        <h3 class="mt-2 mb-2">{{ $item->name }}</h3>
+        <h3 class="mt-2 mb-2">{{ $item->name }}
+          <span class="pl-2"> 
+            <button id="edit"><img class="h-6 w-6" src="/editpencil.png"></button>
+            <button id="delete"><img class="h-6 w-6" src="/deletetrash.png"></button>
+          </span> 
+        </h3>
         <label class="text-xs">{{ $item->description }}</label>
         <details>
           <summary>{{ __(':count challenges', ['count' => $item->challenges()->count()]) }}</summary>
