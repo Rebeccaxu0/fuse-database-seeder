ce36ee82cc8ec4f2d9e9baf8dc1f58744dc35fad
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-11-12 17:57:13 -0600

    WIP to make the default app view look good

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,33 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <style type="text/css">
+          @media (max-width: 2000px) {
+            input#nav-toggle:checked~label#show-button {
+              display: none;
+            }
+            input#nav-toggle:checked~label#hide-button {
+              display: flex;
+            }
+            input#nav-toggle:checked~#nav-menu {
+              display: none;
+              border-radius: 6px;
+            }
+            #nav-menu {margin-top:4rem;}
+          }
+        </style>
 
         @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body>
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
