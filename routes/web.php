add2427596dc2400431f1ae6c767b37c231382ca
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-11-12 17:38:30 -0600

    group and name package routes; replace reference to slug with id

--- routes/web.php
+++ routes/web.php
@@ -59,13 +59,16 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function() {
     ->name('admin.')
     ->group(function () {
     Route::get('/packages', [PackageController::class, 'index'])->name('packages');
-    Route::get('/packages/{package:slug}', [PackageController::class, 'show'])->name('package');
-    Route::get('/addpackage', [PackageController::class, 'create'])->name('addpackage');
-    Route::post('/addpackage', [PackageController::class, 'store']);
-    Route::get('/{package:slug}/editpackage', [PackageController::class, 'edit'])->name('editpackage');
-    Route::put('/{package:slug}/editpackage', [PackageController::class, 'update']);
-    Route::delete('/packages/{package:slug}', [PAckageController::class, 'destroy']);
-    
+    Route::prefix('package')
+      ->name('package.')
+      ->group(function() {
+        Route::get('/add', [PackageController::class, 'create'])->name('add');
+        Route::post('/add', [PackageController::class, 'store']);
+        Route::get('/{package:id}', [PackageController::class, 'show'])->name('view');
+        Route::get('/{package:id}/edit', [PackageController::class, 'edit'])->name('edit');
+        Route::put('{package:id}/edit', [PackageController::class, 'update']);
+        Route::delete('{package:id}/delete', [PackageController::class, 'destroy'])->name('delete');
+      });
     Route::get('/districts', [DistrictController::class, 'index'])->name('districts');
   });
 });
