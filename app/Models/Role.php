da07cf3f5fff655e6040f979388e1cf9f09d3000
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-11-23 16:02:03 -0600

    Thinking about hte future of roles. Let's not add anymore, huh?

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,15 +11,16 @@ class Role extends Model
     use HasFactory;
     use SoftDeletes;
 
-    public const ROOT_ID              = 1;
-    public const ADMIN_ID             = 2;
-    public const REPORT_VIEWER_ID     = 3;
-    public const CHALLENGE_AUTHOR_ID  = 4;
-    public const SUPER_FACILITATOR_ID = 5;
-    public const FACILITATOR_ID       = 6;
-    public const PRE_FACILITATOR_ID   = 7;
-    public const STUDENT_ID           = 8;
-    public const ANONYMOUS_STUDENT_ID = 9;
+    public const ROOT_ID                  = 1;
+    public const ADMIN_ID                 = 2;
+    public const REPORT_VIEWER_ID         = 3;
+    public const CHALLENGE_AUTHOR_ID      = 4;
+    public const SUPER_FACILITATOR_ID     = 5;
+    public const PRE_SUPER_FACILITATOR_ID = 6;
+    public const FACILITATOR_ID           = 7;
+    public const PRE_FACILITATOR_ID       = 8;
+    public const STUDENT_ID               = 9;
+    public const ANONYMOUS_STUDENT_ID     = 10;
 
     /**
      * The users associated with this role.
