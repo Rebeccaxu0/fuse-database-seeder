006beb400a8f02608f6b1ad1a442bd671a6a2c60
Colin Sheaff <colin-sheaff@northwestern.edu>
2021-12-07 16:48:21 -0600

    fix routes on dashboard

--- resources/views/components/explore-tile.blade.php
+++ resources/views/components/explore-tile.blade.php
@@ -1,7 +1,7 @@
 @props(['started' => 0, 'total' => 0])
 <div>
   {{ __('You have tried :started/:total challenges', ['started' => $started, 'total' => $total]) }}
-  <a href="{{ route('challenges') }}">
+  <a href="{{ route('student.challenges') }}">
     Explore
   </a>
 </div>
