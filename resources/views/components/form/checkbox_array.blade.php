5abad04fbfc101a08b64b49abc8c1c01452c2094
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-01-28 09:49:52 -0600

    remove unneeded markup

--- resources/views/components/form/checkbox_array.blade.php
+++ resources/views/components/form/checkbox_array.blade.php
@@ -1,15 +1,13 @@
 @props(['name', 'value', 'label', 'active' => null])
 
 <div>
-    <div class="inline-flex items-center" id="{{ $name }}">
-        <input type="checkbox" class="form-checkbox" id="{{ "{$name}-{$value}" }}" name="{{ $name }}[]"
-            value="{{ $value }}"
-            {{ (old($name) && in_array($value, old($name))) || $active ? 'checked' : '' }}>
-        <label for="{{ "{$name}-{$value}" }}">
-            <span class="mx-2 text-gray-700">{{ $label }}</span>
-        </label>
-        @error($name)
-            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-        @enderror
-    </div>
+    <input type="checkbox" class="form-checkbox" id="{{ "{$name}-{$value}" }}" name="{{ $name }}[]"
+        value="{{ $value }}"
+        {{ (old($name) && in_array($value, old($name))) || $active ? 'checked' : '' }}>
+    <label for="{{ "{$name}-{$value}" }}">
+        <span class="mx-2 text-gray-700">{{ $label }}</span>
+    </label>
+    @error($name)
+        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+    @enderror
 </div>
