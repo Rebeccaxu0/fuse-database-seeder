55665bec8d74a5d6c597a334d6d93e0dfca45a1e
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-01-28 09:54:28 -0600

    move to app template

--- resources/views/admin/package/create.blade.php
+++ resources/views/admin/package/create.blade.php
@@ -1,10 +1,10 @@
-<x-admin-layout>
+<x-app-layout>
 
     <x-slot name="title">{{ __('Create Package') }}</x-slot>
 
     <x-slot name="header">{{ __('Create Package') }}</x-slot>
 
-    <form class="w-full max-w-lg mt-6" action="{{ route('admin.packages.store') }}" method="POST">
+    <form class="mt-6" action="{{ route('admin.packages.store') }}" method="POST">
         @csrf
         <x-form.input label="Title" name="name" required="true" />
         <x-form.textarea name="description">{{ old('description', '') }}</x-form.textarea>
@@ -13,9 +13,11 @@
         <div class="-mx-3 mb-2">
             <label class="text-gray-700 mb-2 form-required">{{ __('Allowed Challenges') }}</label>
         </div>
+        <div class="grid md:grid-cols-2 lg:grid-cols-3">
         @foreach ($challenges as $challenge)
             <x-form.checkbox_array name="challenges" :value="$challenge->id" :label="$challenge->name" />
         @endforeach
+        </div>
         <div class="flex flex-wrap mt-4 -mx-3 mb-2">
             <button type="submit" id="btn-submit" class="text-md h-12 px-6 m-2 bg-fuse-green rounded-lg text-white">
                 Create Package
@@ -23,4 +25,4 @@
         </div>
     </form>
 
-</x-admin-layout>
+</x-app-layout>
