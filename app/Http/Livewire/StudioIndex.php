de78f73b107ee5d22a6aa0a02119e3523e34811e
Emma Mansell <emilyjmansell@gmail.com>
2022-01-28 21:57:18 +0100

    studio page before layout fix

---
+++ app/Http/Livewire/StudioIndex.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\District;
+use App\Models\School;
+use App\Models\Studio;
+
+class StudioIndex extends Component
+
+{
+    public $showStudios = false;
+
+    protected $listeners = ['districtSelected' => 'setDistrict', 'schoolSelected'=> 'setSchool'];
+
+    public function setDistrict(District $district)
+    {
+        $this->setdistrict = $district;
+        $this->showStudios = true;
+        $this->setSchool($this->setdistrict->schools->first());
+        return $this->setdistrict;
+    }
+
+    public function removeDistrict()
+    {
+        $this->setdistrict = null;
+    }
+
+    public function setSchool(School $school)
+    {
+        $this->setschool = $school;
+        $this->showStudios = true;
+        // if ($this->setschool->district->first() != $this->setdistrict){
+        //$this->setdistrict = $this->setschool->district->first();
+        //}
+        return $this->setschool;
+    }
+
+    public function removeSchool()
+    {
+        $this->setschool = null;
+    }
+
+    public function __construct() {
+    
+        $this->setdistrict = $this->setDistrict(District::all()->first());
+        $this->setschool = $this->setSchool($this->setdistrict->schools()->first());
+
+    }
+
+
+    public function render()
+    {
+        return view('livewire.studio-index', [
+            'studios' => Studio::all(),
+        ]);
+    }
+}
