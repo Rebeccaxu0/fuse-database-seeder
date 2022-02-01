1f1ccac1c94dd5066d6ea521d386e365dcefa269
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-02-01 16:50:27 -0600

    ensure no logic for impersonation is run by those who shouldn't

--- app/Http/Livewire/Impersonate.php
+++ app/Http/Livewire/Impersonate.php
@@ -14,22 +14,24 @@ class Impersonate extends Component
 
     public function render()
     {
-        $users_q = User::with(['activeStudio', 'activeStudio.school'])
-            ->where(function ($query) {
-            $query->where('name', 'like', "%{$this->search}%")
-                ->orWhere('full_name', 'like', "%{$this->search}%")
-                ->orWhere('email', 'like', "%{$this->search}%");
-            })
-            ->limit(10);
+        if (Auth::user()->canImpersonate()) {
+            $users_q = User::with(['activeStudio', 'activeStudio.school'])
+                ->where(function ($query) {
+                $query->where('name', 'like', "%{$this->search}%")
+                    ->orWhere('full_name', 'like', "%{$this->search}%")
+                    ->orWhere('email', 'like', "%{$this->search}%");
+                })
+                ->limit(10);
 
-        // If we let facilitators or superfacilitators use this, we will limit
-        // them to searching for member students of the active studio.
-        if (! Auth::user()->is_admin()) {
-          $users_q = $users_q->whereRelation('roles', 'id', Role::STUDENT_ID)
-            ->whereRelation('studios', 'id', Auth::user()->activeStudio->id);
+            // If we let facilitators or superfacilitators use this, we will limit
+            // them to searching for member students of the active studio.
+            if (! Auth::user()->is_admin()) {
+              $users_q = $users_q->whereRelation('roles', 'id', Role::STUDENT_ID)
+                ->whereRelation('studios', 'id', Auth::user()->activeStudio->id);
+            }
+
+            $this->users = $users_q->get();
         }
-
-        $this->users = $users_q->get();
         return view('livewire.impersonate');
     }
 }
