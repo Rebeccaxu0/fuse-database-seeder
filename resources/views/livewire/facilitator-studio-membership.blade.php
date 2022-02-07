2bbb15e6fb1ace652622280cd2ada091888d706a
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-02-07 11:38:51 -0600

    minor tweaks to studio membership page

--- resources/views/livewire/facilitator-studio-membership.blade.php
+++ resources/views/livewire/facilitator-studio-membership.blade.php
@@ -1,6 +1,6 @@
 <div>
-  <h2>{{ __('Students (:count)', ['count' => $students->count()])}}</h2>
-  <div class="grid lg:grid-cols-2">
+  <h2 class="mb-8 text-left">{{ __('Students (:count)', ['count' => $students->count()])}}</h2>
+  <div class="grid lg:grid-cols-2 gap-4">
     @forelse ($students as $student)
     <div class="border rounded-xl px-4 py-2 relative">
       <span class="font-bold">{{ $student->full_name }}</span> <span>{{ $student->name }}</span>
@@ -10,17 +10,23 @@
       </div>
     </div>
     @empty
-    {{ __('Empty Studio.') }}
+    {{ __('No Students') }}
     @endforelse
   </div>
   <livewire:add-student-to-studio-by-search />
 
-  <h2>{{ __('Facilitators (:count)', ['count' => $facilitators->count()])}}</h2>
-  <div class="grid md:grid-cols-2">
+  <h2 class="mb-8 text-left">{{ __('Facilitators (:count)', ['count' => $facilitators->count()])}}</h2>
+  <div class="grid md:grid-cols-2 gap-4">
     @forelse ($facilitators as $facilitator)
-    <div>{{ $facilitator->name }}</div>
+    <div class="border rounded-xl px-4 py-2 relative">
+      <span class="font-bold">{{ $facilitator->full_name }}</span> <span>{{ $facilitator->name }}</span> <span>&lt;{{ $facilitator->email }}&gt;</span>
+      <div class="absolute right-0 top-0 bottom-0">
+        <!-- <livewire:student&#45;edit&#45;modal :student="$facilitator" :wire:key="'edit&#45;' . $facilitator&#45;>id" /> -->
+        <!-- <livewire:student&#45;delete&#45;modal :student="$facilitator" :wire:key="'delete&#45;' . $facilitator&#45;>id" /> -->
+      </div>
+    </div>
     @empty
-    {{ __('Empty Studio.') }}
+    {{ __('No Facilitators') }}
     @endforelse
   </div>
 </div>
