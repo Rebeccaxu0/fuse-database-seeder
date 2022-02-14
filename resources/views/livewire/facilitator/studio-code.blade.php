563d462afc976aebc006cb3542f4a63a3867a3c8
Emma Mansell <emilyjmansell@gmail.com>
2022-02-14 18:27:34 +0100

    studio edit page complete

--- resources/views/livewire/facilitator/studio-code.blade.php
+++ resources/views/livewire/facilitator/studio-code.blade.php
@@ -2,5 +2,5 @@
   <div class="text-xl text-fuse-teal-dk mb-2">
     {!! __("Your Studio Code is '<span class='font-bold uppercase'>:code</span>'", ['code' => $studio->join_code]) !!}
   </div>
-  <button wire:click="setNewStudioCode">{{ __('Refresh your code') }}</button>
+  <button type="button" wire:click="setNewStudioCode">{{ __('Refresh your code') }}</button>
 </div>
