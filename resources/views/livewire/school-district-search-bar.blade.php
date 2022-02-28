49c9d0f75e48836f8414627772875765a33774de
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-02-28 15:12:38 -0600

    fix for school and district search

--- resources/views/livewire/school-district-search-bar.blade.php
+++ resources/views/livewire/school-district-search-bar.blade.php
@@ -1,9 +1,9 @@
 <div class="relative">
     <input id="school_district_search" name="school_district_search" type="text" class="form-input mt-1 block w-full rounded"
-        placeholder="Search schools or districts&hellip;" value="{{ $query }}" wire:keydown.enter.prevent=""
+                                                                                 placeholder="{!! __('Search schools or districts&hellip;') !!}" value="{{ $query }}" wire:keydown.enter.prevent=""
         wire:model.debounce.300ms="query" />
     <div wire:loading class="absolute z-10 w-full bg-white shadow-lg">
-        <div class="list-item">Searching...</div>
+        <div class="list-item">{!! __('Searching&hellip;') !!}</div>
     </div>
 
     @if (!empty($query))
@@ -11,7 +11,7 @@
 
         <div class="relative z-10 w-full bg-white rounded-t-none shadow-lg list-group">
             @forelse ($dss as $ds)
-                <div class="mb-6" wire:click="selectDs({{ $ds->id }})">{{ $ds->district->name }} - {{ $ds->name }}</div>
+            <div class="mb-6" wire:click="selectDs({{ $ds->id }})">{{ $ds->name }} [{{ $ds->district ? $ds->district->name : __('<No District>')}}]</div>
             @empty
                 <div>No results</div>
             @endforelse
