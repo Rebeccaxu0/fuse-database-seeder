c2407af065ab53ad8b19cacd81b64e343c086980
Emma Mansell <emilyjmansell@gmail.com>
2022-03-02 17:27:46 +0100

    cleaning up package index

--- resources/views/admin/package/index.blade.php
+++ resources/views/admin/package/index.blade.php
@@ -8,14 +8,14 @@
     </a>
 
     <div class="md:grid md:grid-cols-2 xl:grid-cols-3 gap-4">
-    @foreach ($packages as $package)
-        <div class="bg-gray-200 rounded-md p-2 mb-2">
+        @foreach ($packages as $package)
+            <div class="bg-gray-200 rounded-md p-2 mb-2">
                 <div class="float-right pl-2">
                     <a href="{{ route('admin.packages.edit', $package->id) }}">
                         <button><img class="h-6 w-6" src="/editpencil.png"></button>
                     </a>
                     <a href="{{ route('admin.packages.copy', $package) }}">
-                        <button type="button" class="bg-fuse-teal visited:bg-violet-700"><img class="h-6 w-6" src="/copyicon.png"></button>
+                        <button><img class="h-6 w-6" src="/copyicon.png"></button>
                     </a>
                     <form method="post" action="{{ route('admin.packages.destroy', $package->id) }}"
                         class="inline-block">
@@ -24,30 +24,31 @@
                         <button type="submit"><img class="h-6 w-6" src="/deletetrash.png"></button>
                     </form>
                 </div>
-            <h3 class="mt-2 mb-2">{{ $package->name }}</h3>
-            @if ($package->student_activity_tab_access)
-                <div>(Student Activity Tab Active)</div>
-              @endif
+                <h3 class="mt-2 mb-2">{{ $package->name }}</h3>
+                @if ($package->student_activity_tab_access)
+                    <div>(Student Activity Tab Active)</div>
+                @endif
 
-            <label class="text-xs">{{ $package->description }}</label>
-            <details>
-                <summary class="cursor-pointer">{{ __(':count challenges', ['count' => count($package->challenges)]) }}</summary>
-                <ol class="list-none">
-                    @foreach ($package->challenges as $challenge)
-                        <li class="list-none text-xs text-fuse-teal">{{ $challenge->name }}</li>
-                    @endforeach
-                </ol>
-            </details>
+                <label class="text-xs">{{ $package->description }}</label>
+                <details>
+                    <summary class="cursor-pointer">
+                        {{ __(':count challenges', ['count' => count($package->challenges)]) }}</summary>
+                    <ol class="list-none">
+                        @foreach ($package->challenges as $challenge)
+                            <li class="list-none text-xs text-fuse-teal">{{ $challenge->name }}</li>
+                        @endforeach
+                    </ol>
+                </details>
 
-            <div class="text-xs">
-                {{ __('Used by :district_count districts, :school_count schools, and :studio_count studios', [
-                    'district_count' => count($package->districts),
-                    'school_count' => count($package->schools),
-                    'studio_count' => count($package->studios),
-                ]) }}
+                <div class="text-xs">
+                    {{ __('Used by :district_count districts, :school_count schools, and :studio_count studios', [
+                        'district_count' => count($package->districts),
+                        'school_count' => count($package->schools),
+                        'studio_count' => count($package->studios),
+                    ]) }}
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
     </div>
 
-</x-admin-layout>
+    </x-admin-layout>
