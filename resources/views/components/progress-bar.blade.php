7b93023535ffa821ef7d8be38adb0ade6a791fe6
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-03-10 09:52:14 -0600

    overflow: clip unsupported on Safari, Opera and others. switch to overflow-x: hidden

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,4 +1,4 @@
-<div class="flex gap-6 h-full rounded-lg overflow-clip">
+<div class="flex gap-6 h-full rounded-lg overflow-x-hidden">
     @foreach ($levels as $level)
         <div aria-label="{{ __('Level :number', ['number' => $level->level_number]) }}" class="progress-bar-segment {{ $level->status }}">@if ($interactive)
         <a href="{{ route('student.level', ['challengeVersion' => $challengeVersion, 'level' => $level]) }}" class="block w-full h-full" ></a>
