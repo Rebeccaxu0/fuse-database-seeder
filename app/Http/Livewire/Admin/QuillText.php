51c84437cfd065ab3176325d95023656f4e396b2
Emma Mansell <emansell@dhcp-129-105-107-164.sesp.northwestern.edu>
2022-04-21 13:15:17 -0500

    putting a pin in quill to work on levels

--- app/Http/Livewire/Admin/QuillText.php
+++ app/Http/Livewire/Admin/QuillText.php
@@ -1,11 +1,24 @@
 <?php
 
 namespace App\Http\Livewire\Admin;
+
+use App\Models\ChallengeVersion;
 use Livewire\Component;
 
 class QuillText extends Component
 {
     public $label;
+    public $sublabel;
+    public ChallengeVersion $challengeversion;
+
+    protected $rules = [
+        'challengeversion.facilitator_notes' => 'optional|string|max:1000',
+    ];
+
+    public function updatedChallengeVersion($name, $key)
+    {
+        $this->challengeversion->save();
+    }
 
     public function render()
     {
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,4 +1,4 @@
-@props(['name', 'sublabel' => false, 'required' => false])
+@props(['name', 'sublabel', 'required' => false])
 
 <div class="mb-6">
     <label class="text-base" for="{{ $name }}">
@@ -7,16 +7,13 @@
             <span class="form-required" title="This field is required.">*</span>
         @endif
     </label>
-    @if ($sublabel)
-    <br/>
-    <span class="text-gray-700 text-xs mb-2">{{ $sublabel }}</span>
-    @endif
+    <p class="text-gray-700 text-xs mt-1">{{ $sublabel }}</p>
     <textarea class="form-textarea block w-full rounded" id="{{ $name }}" name="{{ $name }}"
         @if ($required)
             required
             @endif
             {{ $attributes }}
-            rows="3"
+            rows="2"
   >{{ $slot ?? old($name) }}</textarea>
     @error($name)
         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
