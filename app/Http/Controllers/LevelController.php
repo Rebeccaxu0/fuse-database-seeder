7aaa77225d1e507f487c694aafd30048202b5742
Emma Mansell <emansell@dhcp-129-105-107-237.sesp.northwestern.edu>
2022-04-26 14:54:36 -0500

    level CRUD functions working, challenge versions index reconfigured

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -31,9 +31,11 @@ class LevelController extends Controller
      */
     public function create()
     {
-        return view('admin.level.create', ['parents' => ChallengeVersion::all()->sortBy('name')]);
+        $parents = ChallengeVersion::all()->sortBy('name');
+        $test = ChallengeVersion::first()->id;
+        return view('admin.level.create', ['parents' => $parents]);
     }
-    
+
     /**
      * Store a newly created resource in storage.
      *
@@ -43,15 +45,31 @@ class LevelController extends Controller
     public function store(Request $request)
     {
         $request->flash();
-        $validated = $request->validate([
-            'name' => 'required|unique:levels|max:255',
-        ]);
 
         $level = Level::create([
-            'name' => $request->name,
+            'levelable_id' => $request->levelable_id,
+            'levelable_type' => 'App\Models\ChallengeVersion',
+            'blurb' => $request->blurb,
+            'challenge_desc' => $request->challenge_desc,
+            'stuff_you_need_desc' => $request->syn_desc,
+            'get_started_desc' => $request->gs_desc,
+            'how_to_complete_desc' => $request->htc_desc,
+            'get_help_desc' => $request->gh_desc,
+            'power_up_desc' => $request->pu_desc,
         ]);
 
-        return redirect(route('admin.levels.index'));
+        $level->save;
+
+        $order = [];
+        $i = 0;
+        foreach ($level->levelable->levels()->get() as $level) {
+            $i++;
+            $order[$level->id] = $i;
+        }
+        $level->level_number = $level->levelable->setLevelsOrder($order);
+        $level->save;
+
+        return redirect(route('admin.challengeversions.edit', $request->levelable_id));
     }
 
     /**
@@ -184,7 +202,15 @@ class LevelController extends Controller
     public function update(Request $request, Level $level)
     {
         $level->update([
-            'name' => $request->name,
+            'levelable_id' => $request->levelable_id,
+            'levelable_type' => 'App\Models\ChallengeVersion',
+            'blurb' => $request->blurb,
+            'challenge_desc' => $request->challenge_desc,
+            'stuff_you_need_desc' => $request->syn_desc,
+            'get_started_desc' => $request->gs_desc,
+            'how_to_complete_desc' => $request->htc_desc,
+            'get_help_desc' => $request->gh_desc,
+            'power_up_desc' => $request->pu_desc,
         ]);
 
         return redirect(route('admin.levels.index'));
