4c39d1278309aa0a423d0dd305c281d1e3247076
Emma Mansell <emansell@dhcp-129-105-107-133.sesp.northwestern.edu>
2022-04-29 12:57:12 -0500

    quill form text WIP

--- resources/views/admin/level/create.blade.php
+++ resources/views/admin/level/create.blade.php
@@ -1,15 +1,30 @@
 <x-app-layout>
+    <link href="https://cdn.quilljs.com/1.0.0/quill.snow.css" rel="stylesheet" />
+    <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>
     <x-slot name="title">{{ __('Create Level') }}</x-slot>
 
     <x-slot name="header">{{ __('Create Level') }}</x-slot>
 
-    <form class="mt-6" action="{{ route('admin.levels.store') }}" method="POST">
+    <form id="frm" class="mt-6" action="{{ route('admin.levels.store') }}" method="POST">
         @csrf
         <p> //preview image </p>
-        <x-form.textarea name="The Challenge QUILL">{{ old('description', '') }}</x-form.textarea>
+
+        <livewire:admin.quill-text name="challenge_desc" label="The Challenge" />
+
         <x-form.dropdown label="Parent Challenge" required="true" name="levelable_id" :value="old('levelable_id')" :list="$parents" />
+
         <p> //stuff you need images </p>
-        <x-form.textarea name="Stuff You Need QUILL">{{ old('description', '') }}</x-form.textarea>
+
+        <livewire:admin.quill-text name="syn_desc" label="Stuff You Need" />
+
+        <livewire:admin.quill-text name="gs_desc" label="Get Started">
+
+        <livewire:admin.quill-text name="htc_desc" label="How To Complete">
+
+        <livewire:admin.quill-text name="gh_desc" label="Get Help">
+
+        <livewire:admin.quill-text name="pu_desc" label="Power Up">
+
         <div class="flex flex-wrap mt-4 -mx-3 mb-2">
             <button type="submit" id="btn-submit" class="text-md h-12 px-6 m-2 bg-fuse-green rounded-lg text-white">
                 Create Level
--- resources/views/livewire/admin/quill-text.blade.php
+++ resources/views/livewire/admin/quill-text.blade.php
@@ -1,29 +1,44 @@
-@push('styles')
-<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-@endpush
+<div class="mt-8">
+  <!-- Include Quill stylesheet -->
+  <link href="https://cdn.quilljs.com/1.0.0/quill.snow.css" rel="stylesheet">
+  <!-- Hidden input for form submission (request parameter) -->
+  <input type="hidden" id="{{ $name }}" name="{{ $name }}" />
 
-@push('scripts')
-<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-@endpush
 
-<span class="text-gray-700 mb-2">{{ $label }}</span>
-@if ($sublabel)
-<p class="text-gray-700 text-xs mt-1">{{ $sublabel }}</p>
-@endif
-<div class="quill bg-white mt-2 mb-6" wire:ignore>
-  <div x-data x-ref="quillEditor" x-init="
-         toolbarOptions = ['bold', 'italic', 'underline', 'link'];
-         options = {
-            modules: {
-                toolbar: toolbarOptions
-            },
-            theme: 'snow'
-         };
-         quill = new Quill($refs.quillEditor, options);
-         quill.on('text-change', function () {
-           $dispatch('quill-input', quill.root.innerHTML);
-         });
-       " x-on:quill-input.debounce.1000ms="@this.set('challengeversion.facilitator_notes', $event.detail)">
-    {!! $challengeversion->facilitator_notes !!}
+  <span class="text-gray-700 mb-2">{{ $label }}</span>
+
+  <!-- Create the toolbar container -->
+  <div id="toolbar">
   </div>
+
+  <!-- Create the editor container -->
+  <div id="editor">
+  </div>
+  <br />
+  <!-- Include the Quill library -->
+  <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>
+
+  <!-- Initialize Quill editor -->
+  <script>
+    var quill = new Quill('#editor', {
+      modules: {
+        toolbar: [
+          ['bold', 'italic'],
+          ['link', 'blockquote', 'code-block', 'image'],
+          [{
+            list: 'ordered'
+          }, {
+            list: 'bullet'
+          }]
+        ]
+      },
+      theme: 'snow'
+    });
+
+    // Set value for form submission
+    var content = document.getElementById("{{ $name }}");
+    quill.on('text-change', function() {
+      content.value = quill.root.innerHTML;
+    });
+  </script>
 </div>
