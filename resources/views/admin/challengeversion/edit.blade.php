509f43867f91a44e60b5c9e4bcf4472409c23acf
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-05-11 21:44:56 -0500

    putting a pin in quill to work on levels

--- resources/views/admin/challengeversion/edit.blade.php
+++ resources/views/admin/challengeversion/edit.blade.php
@@ -7,23 +7,47 @@
     <form class="mt-6" action="{{ route('admin.challengeversions.update', $challengeversion) }}" method="POST">
         @method('PUT')
         @csrf
-        <x-form.input label="Name" name="name" required="true" :value="old('name', $challengeversion->name)"/>
-        <x-form.textarea name="version description"/>
-        <x-form.textarea name="blurb" :value="old('name', $challengeversion->blurb)"/>
+        <x-form.input label="Name" name="name" required="true" :value="old('name', $challengeversion->name)" />
         <x-form.dropdown label="Category" required="true" name="category_id" :value="old('challenge_category_id', $challengeversion->challenge_category_id)" :list="$categories" />
-        <x-form.dropdown label="Prerequisite Challenge" :value="old('prerequisite_challenge_version_id', $challengeversion->prerequisite_challenge_version_id)" name="prereqchal" :list="$challenges" />
-        <x-form.input label="Information Article URL" name="infourl" :value="old('info_article_url', $challengeversion->info_article_url)"/>
-        <livewire:admin.quill-text label="Gallery Description">
-        
-        <div class="flex flex-wrap mt-4 -mx-3 mb-2">
-            <button type="submit" id="btn-submit"
-                class="text-md h-12 px-6 m-2 bg-fuse-green rounded-lg text-white">{{ __('Update Challenge Version') }}</button>
-        </div>
+        <p> //preview image </p>
+        <p> //gallery media </p>
+        <p> //preview video </p>
+        <x-form.textarea name="version description" sublabel="A short description to help differentiate between different versions of the same challenge." />
+
+        <livewire:admin.quill-text name="blurb" label="Gallery Blurb" sublabel="ex. 'Design your own 3D balance toy.'" :challengeversion="$challengeversion">
+            <livewire:admin.quill-text name="summary" label="Summary" :challengeversion="$challengeversion">
+                <livewire:admin.quill-text name="stuffyouneed" label="Stuff You Need" sublabel="ex. 'Chromebook, LED lights.'" :challengeversion="$challengeversion">
+                    <livewire:admin.quill-text name="facnotes" label="Facilitator Notes" :challengeversion="$challengeversion">
+                        <livewire:admin.quill-text name="chromeinfo" label="Chromebook Info" :challengeversion="$challengeversion">
+                            <div class="quill bg-white mt-2 mb-6">
+                                <input name="test" type="hidden">
+                                <div id="editor">
+                                    <p> test </p>
+                                </div>
+                            </div>
+                            <x-form.dropdown label="Prerequisite Challenge" :value="old('prerequisite_challenge_version_id', $challengeversion->prerequisite_challenge_version_id)" name="prereqchal" :list="$challenges" />
+                            <x-form.input label="Information Article URL" name="infourl" :value="old('info_article_url', $challengeversion->info_article_url)" />
+                            <div class="flex flex-wrap mt-4 -mx-3 mb-2">
+                                <button type="submit" id="btn-submit" class="text-md h-12 px-6 m-2 bg-fuse-green rounded-lg text-white">{{ __('Save Challenge Version') }}</button>
+                            </div>
     </form>
 
-    <script>
-        function checkBoxes() {
-            if ($('#license_status').is(":checked")) $('#anonymize').prop("disabled", true).prop("checked", false);
-        }
-    </script>
+    <!--<script src="https://cdn.quilljs.com/1.3.6/quill.js">
+        $var quill = new Quill('#editor', {
+            options = {
+                modules: {
+                    toolbar: toolbarOptions
+                },
+                theme: 'snow'
+            }
+        });
+
+        var form = document.querySelector('form');
+        form.onsubmit = function() {
+            // Populate hidden form on submit
+            var tester = document.querySelector('input[name=test]');
+            tester.value = JSON.stringify(quill.getContents());
+        };
+    </script>-->
+
 </x-app-layout>
--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,4 +1,4 @@
-@props(['name', 'sublabel' => false, 'required' => false])
+@props(['name', 'sublabel', 'required' => false])
 
 <div class="mb-6">
     <label class="text-base" for="{{ $name }}">
@@ -7,15 +7,13 @@
             <span class="form-required" title="This field is required.">*</span>
         @endif
     </label>
-    @if ($sublabel)
     <p class="text-gray-700 text-xs mt-1">{{ $sublabel }}</p>
-    @endif
     <textarea class="form-textarea block w-full rounded" id="{{ $name }}" name="{{ $name }}"
         @if ($required)
             required
             @endif
             {{ $attributes }}
-            rows="3"
+            rows="2"
   >{{ $slot ?? old($name) }}</textarea>
     @error($name)
         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
