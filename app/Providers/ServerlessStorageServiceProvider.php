1d5535f54018a3f88fccc58f302b233d2a5632ce
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-06-12 19:01:28 -0500

    new ServiceProvider to make sure all necessary directories in /tmp are created for serverless hosting

---
+++ app/Providers/ServerlessStorageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServerlessStorageServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (in_array(env('SERVER_SOFTWARE'), ['bref', 'vapor'])) {
+            $path = '/tmp/storage';
+            $directories = [
+                '/app',
+                '/cache',
+                '/framework/cache',
+                '/framework/views',
+                '/bootstrap/cache',
+            ];
+            foreach ($directories as $directory) {
+                if (! is_dir($path . $directory)) {
+                    mkdir($path . $directory, 0755, true);
+                }
+            }
+            $this->app->useStoragePath($path);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        App\Providers\ServerlessStorageServiceProvider::class,
         App\Providers\SocialstreamServiceProvider::class,
         App\Providers\LtiServiceProvider::class,
 
