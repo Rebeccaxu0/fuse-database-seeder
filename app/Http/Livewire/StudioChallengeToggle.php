df66696bb55573c5c75a90d006e2dd3f94065403
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-06-13 19:42:31 -0500

    refactor Livewire component to avoid certain data types for public class parameters

--- app/Http/Livewire/StudioChallengeToggle.php
+++ app/Http/Livewire/StudioChallengeToggle.php
@@ -8,19 +8,24 @@ use App\Models\Studio;
 
 class StudioChallengeToggle extends Toggle
 {
-    public Studio $studio;
-    public ChallengeVersion $challengeVersion;
+    public bool $is_active = false;
+    public string $event = 'toggle';
+    public string $label = '';
+    public int $studioId;
+    public int $challengeVersionId;
 
     public function mount() {
-        $this->is_active = $this->studio->challengeVersions->contains($this->challengeVersion);
+        $studio = Studio::find($this->studioId);
+        $challengeVersion = ChallengeVersion::find($this->challengeVersionId);
+        $this->is_active = $studio->challengeVersions->contains($challengeVersion);
     }
 
     public function toggle()
     {
-      parent::toggle();
-      $action = $this->is_active ? 'attach' : 'detach';
-      $this->studio->challengeVersions()->$action($this->challengeVersion->id);
+        parent::toggle();
+        $action = $this->is_active ? 'attach' : 'detach';
+        $studio = Studio::find($this->studioId);
+        $studio->challengeVersions()->$action($this->challengeVersionId);
     }
-
 }
 
