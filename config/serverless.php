6b334bf0745fcbdba0a49ae1ba830ac7a13006ce
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-06-24 16:56:03 -0500

    bring serverless configuration in line with tlint suggestions

---
+++ config/serverless.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Key set by Vapor/Bref to determine if running on 'serverless' environment.
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'server_software' => env('SERVER_SOFTWARE', 'local'),
+];
+
