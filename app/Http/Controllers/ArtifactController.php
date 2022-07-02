a066d3f39538a0767c8a358fbce9973fe5a90acd
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-01 23:26:03 -0500

    make sure to sort artifacts by created_by before pagination

--- app/Http/Controllers/ArtifactController.php
+++ app/Http/Controllers/ArtifactController.php
@@ -31,7 +31,7 @@ class ArtifactController extends Controller
      */
     public function artifact_gallery(User $user = null)
     {
-        $artifacts = $user->artifacts()->paginate(12);
+        $artifacts = $user->artifacts()->orderBy('created_at', 'desc')->paginate(12);
         return view('student.my_stuff', ['artifacts' => $artifacts, 'studio' => $user->activeStudio]);
     }
 
