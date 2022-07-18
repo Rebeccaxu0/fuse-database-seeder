b2986a352525447290987f9af3f051d624fe4b8c
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-18 14:21:02 -0500

    make foreign key relation on announcement_read table cascade to allow for forced deletes of announcements

--- database/migrations/2022_05_19_200410_create_announcements_table.php
+++ database/migrations/2022_05_19_200410_create_announcements_table.php
@@ -25,9 +25,13 @@ return new class extends Migration
 
         Schema::create('announcement_seen', function (Blueprint $table) {
             $table->foreignId('announcement_id')
-                  ->constrained();
+                  ->constrained()
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
             $table->foreignId('user_id')
-                  ->constrained();
+                  ->constrained()
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
             $table->primary(['announcement_id', 'user_id']);
