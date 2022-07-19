541fcf528341b87ed6ce7a4a170c61ff4a8519b7
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-18 23:57:46 -0500

    filter out null challenges from started level parents

--- app/Models/User.php
+++ app/Models/User.php
@@ -293,6 +293,7 @@ class User extends Authenticatable
                         ->get()
                         ->unique()
                         ->map(fn($level, $key) => $level->levelable)
+                        ->filter(fn($val) => ! is_null($val->id))
                         ->unique();
         });
     }
