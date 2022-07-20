e4927c1e4e22ce0d02cd529ef43eca53efe77573
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-19 22:44:04 -0500

    mostly remove alpine directives in favor of Blade template directives

--- resources/views/livewire/alum-registration.blade.php
+++ resources/views/livewire/alum-registration.blade.php
@@ -1,3 +1,5 @@
+<x-slot name="title">{{ $title }}</x-slot>
+
 <div class="bg-fuse-teal-500 lg:p-24">
     <x-jet-banner />
     <div class="p-12 bg-transparent rounded-xl shadow-xl border md:container md:p-24">
@@ -6,38 +8,60 @@
                 <img class="object-contain h-96" src="{{ asset('/img/interest.svg') }}">
             </div>
 
-            <div x-data="{ openJ: @entangle('showJoin').defer, openE: @entangle('showEmail').defer }" class="mt-6 grid grid-cols-1 gap-6 max-w-full">
-                <x-slot name="title">{{ __('Lobby') }}</x-slot>
+            <div class="mt-6 grid grid-cols-1 gap-6 max-w-full">
                 <div class="p-12 md:pt-8 pb-8 mx-auto">
                     <div>
                         <span><img class="h-16 rounded-full" src="{{ auth()->user()->getPhotoUrl() }}"></span>
                         <span class="text-xl">{{ auth()->user()->name }}</span>
                     </div>
-                    <h1>{{ __('Welcome Back!') }}</h1>
+                    <h1>{{ $title }}</h1>
                     <div>
-                        <form wire:submit.prevent="codecheck" onkeydown="return event.key != 'Enter';">
+                        <form wire:submit.prevent="submit" onkeydown="return event.key != 'Enter';">
                             <h3>{{ __('Please enter your Studio Code') }}</h3>
-                            <input type="text" class="border-2 border-gray-200 shadow-md rounded block h-10 text-slate-500 w-64 p-2 m-4" name="studio_code" id="studio_code" placeholder="{{ __('TINY WIRE 178') }}" wire:model="studioCode" wire:keyup.debounce.600ms="codecheck" />
+                            <input type="text"
+                                   class="border-2 border-gray-200 shadow-md rounded block h-10 text-slate-500 w-64 p-2 m-4"
+                                   name="studio_code"
+                                   id="studio_code"
+                                   placeholder="{{ __('TINY WIRE 178') }}"
+                                   wire:model="studioCode"
+                                   />
                             @error('studioCode')
                             <span class="text-red-500">
                                 {{ $message }}
                             </span>
                             @enderror
-                            <div x-show="openE">
+                            @if ($showEmail)
+                            <div>
                                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                                <x-jet-input id="email" class="block mt-1 rounded text-slate-500 w-full" type="text" name="email" :value="old('email')" />
+                                <input type="email"
+                                   class="border-2 border-gray-200 shadow-md rounded block h-10 text-slate-500 w-64 p-2 m-4"
+                                   required
+                                   name="email"
+                                   id="email"
+                                   wire:model="email"
+                                   />
+                            @error('email')
+                            <span class="text-red-500">
+                                {{ $message }}
+                            </span>
+                            @enderror
                             </div>
-                            <p class="mt-6" x-show="openJ">{{ __(':studio in :school', ['studio' => $studioName, 'school' => $school]) }}</p>
-                            <button type="submit" x-show="openJ" wire:click="join">{{ __('Join') }}</button>
+                            @endif
+                            @if ($validCode)
+                            <p class="mt-6">{{ __(':studio in :school', ['studio' => $studioName, 'school' => $school]) }}</p>
+                            <button type="submit" wire:click="submit">{{ __('Join') }}</button>
+                            @endif
                         </form>
                     </div>
-                    <div x-show="! openJ">
+                    @if (! $validCode)
+                    <div>
                         <h3>{{ __('No Studio Code?') }}</h3>
                         <ul>
                             <li>{!! __('You can still access your <a href=":mystuff">My Stuff</a>', ['mystuff' => route('student.my_stuff')]) !!}</li>
                             <li>{!! __('Try our <a href="https://www.fusestudio.net/try">Free Trial</a> version of FUSE') !!}</li>
                         </ul>
                     </div>
+                    @endif
                 </div>
             </div>
         </div>
