23339c6ae47dcda2d71e70194485f0d84feb5196
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-19 22:44:35 -0500

    add misspellings to find out where the heck this is used if at all...

--- resources/views/auth/lobby.blade.php
+++ resources/views/auth/lobby.blade.php
@@ -1,8 +1,8 @@
 <x-guest-logo-layout>
-    <x-slot name="title">{{ __('Lobby') }}</x-slot>
+    <x-slot name="title">{{ __('rLobby') }}</x-slot>
     <div class="mt-6 grid grid-cols-1 gap-6 max-w-full">
         <x-jet-validation-errors />
-        <h1 class="mt-6 text-fuse-teal text-3xl font-bold font-display text-left">{{ __('Welcome to FUSE!') }}</h1>
+        <h1 class="mt-6 text-fuse-teal text-3xl font-bold font-display text-left">{{ __('iWelcome to FUSE!') }}</h1>
         <livewire:lobby-join-studio />
         <p class="rounded py-2 text-fuse-dk-teal text-base md:text-sm text-center">
             <a class="ml-6 underline" href="https://www.fusestudio.net/">Free Trial</a>
