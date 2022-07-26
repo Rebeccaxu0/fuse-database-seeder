79aa3e6842f4d91e8ed51d3cccca225b6c1cf4c4
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-07-26 14:26:03 -0500

    don't escape text in the metatitle

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>FUSE{{ strlen($title) ? ': ' . $title : '' }}</title>
+    <title>FUSE{!! strlen($title) ? ': ' . $title : '' !!}</title>
     @stack('styles')
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
