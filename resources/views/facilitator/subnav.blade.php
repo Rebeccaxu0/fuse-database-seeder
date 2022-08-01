cbd591a4ccdef96b87e6bff56ae1d22a3b9d53bc
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-01 16:34:09 -0500

    point to new support page

--- resources/views/facilitator/subnav.blade.php
+++ resources/views/facilitator/subnav.blade.php
@@ -19,8 +19,8 @@
                 href="{{ route('facilitator.settings') }}">{{ __('Settings') }}</a>
         </li>
         <li>
-            <a class="px-2 {{ request()->routeIs('facilitator.announcements') ? 'active' : '' }}"
-                href="{{ route('facilitator.announcements') }}">{{ __('Support') }}</a>
+            <a class="px-2 {{ request()->routeIs('facilitator.support') ? 'active' : '' }}"
+                href="{{ route('facilitator.support') }}">{{ __('Support') }}</a>
         </li>
         @if (Auth::user()->isSuperFacilitator())
         <li>
