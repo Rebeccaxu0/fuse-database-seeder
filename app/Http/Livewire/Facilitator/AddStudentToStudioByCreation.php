45f6bf1baadabf80afad888e31d80b508e2c5357
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-02 15:10:11 -0500

    Make sure all calls to Log include the class

--- app/Http/Livewire/Facilitator/AddStudentToStudioByCreation.php
+++ app/Http/Livewire/Facilitator/AddStudentToStudioByCreation.php
@@ -7,6 +7,7 @@ use App\Models\GenderOptions;
 use App\Models\Studio;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class AddStudentToStudioByCreation extends Component
