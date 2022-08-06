f1b9431cb1738f68229533b392b84f80907a32d3
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-06 09:04:46 -0500

    Edits to Error Pages
    - Make sure all error pages use the FUSE template.
    - Update 503 to better reflect maintenance

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::fuse')
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __('Service Unavailable'))
+@section('message', __('Service Unavailable: Down for Maintenance'))
