dea27f821d74b64c0c09e5d2eaa61f953d124ebf
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-07 12:06:53 -0500

    Should have been bundled w previous commit.

--- app/Events/StudioDeleting.php
+++ app/Events/StudioDeleting.php
@@ -8,7 +8,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StudioDeleted
+class StudioDeleting
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
