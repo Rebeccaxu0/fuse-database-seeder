ffbb02e632a5c562268cb563a910c0477ea3a70b
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-08 11:58:49 -0500

    add 'users' to admin nav

--- resources/views/admin/subnav.blade.php
+++ resources/views/admin/subnav.blade.php
@@ -2,6 +2,10 @@
     <x-subnav-ul id="admin-menu">
         <li>
             <a class="px-2 {{ request()->routeIs('admin.users.index') ? 'active' : '' }}"
+                href="{{ route('admin.users.index') }}">Users</a>
+        </li>
+        <li>
+            <a class="px-2 {{ request()->routeIs('admin.challengeversions.index') ? 'active' : '' }}"
                 href="{{ route('admin.challengeversions.index') }}">Challenges</a>
         </li>
         <li>
