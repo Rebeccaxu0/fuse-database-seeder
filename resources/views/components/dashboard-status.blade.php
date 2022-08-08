0770a7eb0f63184b8657fdcf39439f3c6965bc91
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-08 11:59:30 -0500

    don't merge crazy things into dashboard!

--- resources/views/components/dashboard-status.blade.php
+++ resources/views/components/dashboard-status.blade.php
@@ -1,9 +1,10 @@
-<section {{ $attributes->merge(['class' => 'rounded-xl bg-fuse-teal-dk text-white flex flex-col items-center p-8']) }}>
+<section class='rounded-xl bg-fuse-teal-dk text-white flex flex-col items-center p-8'>
   @if ($explore)
     {{ __('Try a new challenge') }}
   @else
   <div class="text-xs">{{ __('You last worked on:') }}</div>
   <h3 class="text-white">{{ $mostRecent }}</h3>
+  <x-progress-bar :user="$user" :interactive="true" :levelable="$challengeVersion" class="h-4 my-0" />
   @endif
   <a href="{{ $buttonLink }}" class="btn">
     {{ $buttonText }}
