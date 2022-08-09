baaeb1cb6d5696033e6d4b825d3319e22a4e68c4
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-08 23:33:27 -0500

    replace broken image link

--- resources/views/livewire/add-facilitator.blade.php
+++ resources/views/livewire/add-facilitator.blade.php
@@ -6,7 +6,7 @@
             <input type="hidden" name="facilitatorsToAdd[]" value="{{ $id }}"> {{ $user['full_name'] }}
             {{ $user['name'] }}
             <button class="inline-flex" wire:click="removeUser({{ $id }})">
-                <img class="h-6 w-6" src="/deletetrash.svg"/>
+                <x-icon icon="trash" width=25 height=25 class="inline ml-2 text-black" />
             </button>
         </div>
     @endforeach
--- resources/views/livewire/add-school.blade.php
+++ resources/views/livewire/add-school.blade.php
@@ -5,7 +5,7 @@
         <div>
             <input type="hidden" name="schoolsToAdd[]" value="{{ $id }}">{{ $school['name'] }}
             <button class="inline-flex" wire:click="removeUser({{ $id }})">
-                <img class="h-6 w-6" src="/deletetrash.svg"/>
+                <x-icon icon="trash" width=25 height=25 class="inline ml-2 text-black" />
             </button>
         </div>
     @endforeach
