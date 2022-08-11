87198a339b3cc3cb3a600cd93cb6f21620bf3c70
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-08-10 20:48:23 -0600

    use json db columns for translatable content

--- database/migrations/2022_08_07_141723_create_help_articles_table.php
+++ database/migrations/2022_08_07_141723_create_help_articles_table.php
@@ -10,8 +10,8 @@ return new class extends Migration
     {
         Schema::create('help_articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->longText('body');
+            $table->json('name');
+            $table->json('body');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
             $table->softDeletes();
