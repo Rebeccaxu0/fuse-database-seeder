d753ca0b7afac9b6d5b8ed25194fcb2e043cbe95
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-09-13 09:48:38 -0500

    host filestack picker js local to avoid whitelist

--- resources/views/livewire/filestack-picker.blade.php
+++ resources/views/livewire/filestack-picker.blade.php
@@ -1,5 +1,6 @@
 @pushOnce('scripts')
-<script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script>
+{{-- <script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script> --}}
+<script src="/js/filestack.min.js"></script>
 @endPushOnce
 
 @push('scripts')
