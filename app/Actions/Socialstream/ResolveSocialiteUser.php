013b5cd1409587f4763023c659c0f88333f823b0
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-10-03 14:41:11 -0500

    Add logging info to SSO flow

--- app/Actions/Socialstream/ResolveSocialiteUser.php
+++ app/Actions/Socialstream/ResolveSocialiteUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Socialstream;
 
+use Illuminate\Support\Facades\Log;
 use JoelButcher\Socialstream\Contracts\ResolvesSocialiteUsers;
 use JoelButcher\Socialstream\Socialstream;
 use Laravel\Socialite\Facades\Socialite;
@@ -16,7 +17,20 @@ class ResolveSocialiteUser implements ResolvesSocialiteUsers
      */
     public function resolve($provider)
     {
-        $user = Socialite::driver($provider)->user();
+        Log::debug("SSO Login Attempt", [$provider]);
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (Exception $e) {
+            Log::error('SSO Fail (stateful)', $e);
+            try {
+                $user = Socialite::driver($provider)->stateless()->user();
+            } catch (Exception $e) {
+                Log::error('SSO Fail (stateless)', $e);
+                session()->flash('flash.banner', __('Unable to log you in. Please try again.'));
+                return redirect('/login');
+            }
+        }
+        Log::debug("SSO Login Success", [$provider, $user->email]);
 
         if (Socialstream::generatesMissingEmails()) {
             $user->email = $user->getEmail() ?? "{$user->id}@{$provider}" . config('app.domain');
