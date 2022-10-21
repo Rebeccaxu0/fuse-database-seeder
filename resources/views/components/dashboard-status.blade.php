056bab1f58801fe96c8a29dadaf7263f2dd5a325
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-10-21 14:21:55 -0500

    'Try a Challenge' Styling

--- resources/views/components/dashboard-status.blade.php
+++ resources/views/components/dashboard-status.blade.php
@@ -1,6 +1,6 @@
 <section class='rounded-xl bg-fuse-teal-dk text-white flex flex-col items-center p-8'>
   @if ($explore)
-    {{ __('Try a new challenge') }}
+    <h3 class="text-white">{{ __('Try a new challenge') }}</h3>
   @else
   <div class="text-xs">{{ __('You last worked on:') }}</div>
   <h3 class="text-white">{{ $mostRecent }}</h3>
