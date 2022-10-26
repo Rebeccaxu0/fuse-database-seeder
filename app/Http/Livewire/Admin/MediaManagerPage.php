0f5aca4b7f10c1dc675b4f1f6db37e43b90115ba
Colin Sheaff <colin-sheaff@northwestern.edu>
2022-10-26 09:11:21 -0500

    WIP for media page

--- app/Http/Livewire/Admin/MediaManagerPage.php
+++ app/Http/Livewire/Admin/MediaManagerPage.php
@@ -3,22 +3,38 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Media;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
+use MediaUploader;
+use Plank\Mediable\Helpers\File;
+// use Plank\Mediable\Media;
 
 class MediaManagerPage extends Component
 {
+    use WithFileUploads;
     use WithPagination;
 
-    public string $current_dir = '';
+    public string $currentLocation = '';
+    public array $currentLocationArray = [];
+    public array $subdirectories = [];
     public string $fileSearch = '';
     public bool $onlyImages = false;
+    public string $fsdisk = 'public';
+    public string $newSubdir = '';
+    public $fileToUpload;
 
-    protected $listeners = ['toggleImages'];
+    protected $listeners = [
+        'toggleImages',
+         'livewire-upload-finish' => 'upload',
+        ];
     protected $queryString = [
+        'fsdisk' => ['except' => ''],
         'onlyImages' => ['except' => false, 'as' => 'images'],
         'page' => ['except' => 1, 'as' => 'p'],
         'fileSearch' => ['except' => '', 'as' => 's'],
@@ -35,19 +51,139 @@ class MediaManagerPage extends Component
 
     public function mount()
     {
-        Gate::allowIf(auth()->user()->isAdmin());
+        Gate::allowIf(Auth::user()->isAdmin());
+    }
+
+    public function makeSubdir()
+    {
+        if (Str::of($this->newSubdir)->test('/[\w\d]+/')) {
+            $directory = $this->currentLocation . '/' . $this->newSubdir;
+            Storage::disk($this->fsdisk)->makeDirectory($directory);
+            $this->newSubdir = '';
+        }
+    }
+
+    public function upload()
+    {
+        $file = $this->fileToUpload
+            ->storeAs($this->currentLocation, $this->fileToUpload->getClientOriginalName(), $this->fsdisk);
+        MediaUploader::importPath($this->fsdisk, $file);
+    }
+
+    public function setFSDisk(string $fsdisk)
+    {
+        $disks = ['public', 'artifacts'];
+        if (in_array($fsdisk, $disks)) {
+            $this->fsdisk = $fsdisk;
+        }
+    }
+
+    public function navigateBack(int $depth)
+    {
+        if ($depth >= 0) {
+            $targetLocation = '';
+            for ($index = 0; $index <= $depth; $index++) {
+                $targetLocation .= "/{$this->currentLocationArray[$index]}";
+                $targetLocationArray[] = $this->currentLocationArray[$index];
+            }
+            $this->currentLocation = $targetLocation;
+            $this->currentLocationArray = $targetLocationArray;
+        }
+        else {
+            $this->currentLocation = '';
+            $this->currentLocationArray = [];
+        }
+    }
+
+    public function navigateForward(int $subdir_index)
+    {
+        if ($subdir_index < count($this->subdirectories)) {
+            $this->currentLocationArray[] = $this->subdirectories[$subdir_index];
+            $this->currentLocation = implode('/', $this->currentLocationArray);
+        }
+    }
+
+    public function refresh()
+    {
+        $diskFiles = Storage::disk($this->fsdisk)->files($this->currentLocation);
+        $mediaRecords = Media::inDirectory($this->fsdisk, $this->currentLocation)->get();
+        // Emulate Artisan command 'sync' which is just 'prune' then 'import'.
+        // https://github.com/plank/laravel-mediable/blob/master/src/Commands/SyncMediaCommand.php
+        // https://github.com/plank/laravel-mediable/blob/master/src/Commands/PruneMediaCommand.php
+        // https://github.com/plank/laravel-mediable/blob/master/src/Commands/ImportMediaCommand.php
+        foreach ($mediaRecords as $media) {
+            if (! $media->fileExists()) {
+                $media->delete();
+            }
+        }
+        // Update Media Records.
+        $mediaRecords = Media::inDirectory($this->fsdisk, $this->currentLocation)->get();
+        foreach ($diskFiles as $path) {
+            if ($record = $this->getRecordForFile($path, $mediaRecords)) {
+                $this->updateRecordForFile($record, $path);
+            } else {
+                $this->createRecordForFile($this->fsdisk, $path);
+            }
+        }
+    }
+
+    /**
+     * Search through the record list for one matching the provided path.
+     * @param  string $path
+     * @param  Collection $existingMedia
+     * @return Media|null
+     */
+    protected function getRecordForFile(string $path, Collection $existingMedia): ?Media
+    {
+        $directory = File::cleanDirname($path);
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+        $extension = pathinfo($path, PATHINFO_EXTENSION);
+
+        return $existingMedia->filter(function (Media $media) use ($directory, $filename, $extension) {
+            return $media->directory == $directory && $media->filename == $filename && $media->extension == $extension;
+        })->first();
+    }
+
+    /**
+     * Generate a new media record.
+     * @param  string $disk
+     * @param  string $path
+     * @return void
+     */
+    protected function createRecordForFile(string $disk, string $path): void
+    {
+        try {
+            MediaUploader::importPath($disk, $path);
+        } catch (MediaUploadException $e) {
+            $this->warn($e->getMessage(), 'vvv');
+        }
+    }
+
+    /**
+     * Update an existing media record.
+     * @param  \Plank\Mediable\Media $media
+     * @param  string $path
+     * @return void
+     */
+    protected function updateRecordForFile(Media $media, string $path): void
+    {
+        try {
+            MediaUploader::update($media);
+        } catch (MediaUploadException $e) {
+            $this->warn($e->getMessage(), 'vvv');
+        }
     }
 
     public function render()
     {
-        $public = Storage::disk('public');
-        $directories = $public->directories('/' . $this->current_dir);
-        $directories = DB::table('media')
-            ->select('directory')
-            ->distinct()
-            ->orderBy('directory')
-            ->get();
-        $files = Media::inDirectory('public', $this->current_dir);
+        $disk = Storage::disk($this->fsdisk);
+        $fullPathSubdirectories = $disk->directories('/' . $this->currentLocation);
+        $this->subdirectories = [];
+        foreach ($fullPathSubdirectories as $fullpath) {
+            $patharray = explode('/', $fullpath);
+            $this->subdirectories[] = array_pop($patharray);
+        }
+        $files = Media::inDirectory($this->fsdisk, $this->currentLocation);
         if (strlen($this->fileSearch) > 2) {
             $files = $files->where('filename', 'like', "%{$this->fileSearch}%");
         }
@@ -59,9 +195,6 @@ class MediaManagerPage extends Component
         }
         $files = $files->orderBy('filename')
                        ->paginate(15);
-        return view('livewire.admin.media-manager', [
-            'directories' => $directories,
-            'files' => $files,
-        ]);
+        return view('livewire.admin.media-manager', ['files' => $files]);
     }
 }
--- resources/views/livewire/admin/media-manager.blade.php
+++ resources/views/livewire/admin/media-manager.blade.php
@@ -3,26 +3,63 @@
 
     <x-slot name="header">{{ __('Media') }}</x-slot>
 
-    <livewire:toggle label="{{ __('Images Only') }}" event="toggleImages" >
-    @if ($onlyImages)
+    {{-- <livewire:toggle label="{{ __('Images Only') }}" event="toggleImages" > --}}
+    {{-- @if ($onlyImages)
     only images
     @else
     everything
-    @endif
+    @endif --}}
     {{-- <h3>{{ $query }}</h3> --}}
-    <details>
-        <summary>
-            <input class="inline-block" type="text" wire:model="current_dir" placeholder="{{ __('Current Directory') }}">
-        </summary>
-        <ul>
-            @foreach ($directories as $directory)
-            <li class="p-0 m-0">{{ $directory->directory }}</li>
+    {{-- <h2>
+        Disk: <button>Public</button> |
+        <button wire:click="setFSDisk('artifacts')">User Artifacts</button>
+    </h2> --}}
+    <h3>Current Location: {{ $fsdisk }}::<span wire:click="navigateBack(-1)" class="cursor-pointer">&lt;root&gt;</span>
+    @foreach ($currentLocationArray as $k => $currentLocation)
+        /
+        @if (! $loop->last)
+        <span wire:click="navigateBack({{ $k }})" class="cursor-pointer">
+        @endif
+        {{ $currentLocation }}
+        @if (! $loop->last)
+        </span>
+        @endif
+        @endforeach
+        <div wire:loading wire:target='navigateBack, navigateForward'>(NAVIGATING...)</div>
+    </h3>
+    <button wire:click="refresh" class="btn">Refresh files <div wire:loading wire:target="refresh">(Working...)</div></button>
+    <form wire:submit.prevent="makeSubdir">
+        <fieldset class="border-black border p-2 max-h-64 overflow-y-scroll">
+            <legend>Subdirectories</legend>
+            <label class="flex items-center gap-2 border border-black rounded-lg">
+                <span>Make new Subdirectory</span>
+                <input type="text" wire:model="newSubdir" placeholder="subdirectory name" class="w-1/2">
+                <button type="submit">Make new Subdirectory</button>
+            </label>
+            @foreach ($subdirectories as $k => $subdirectory)
+            <span wire:click="navigateForward({{ $k }})" class="p-2 m-1 inline-block rounded-lg bg-fuse-teal-dk text-white hover:bg-fuse-teal cursor-pointer">{{ $subdirectory }}</span>
             @endforeach
-        </ul>
-    </details>
+        </fieldset>
+    </form>
     <input wire:model.debounce.300ms="fileSearch" type="text" placeholder="{{ __('Filter by filename') }}">
+    <form wire:submit.prevent="uploadFile"
+        x-data="{ isUploading: false, progress: 0 }"
+        x-on:livewire-upload-start="isUploading = true"
+        x-on:livewire-upload-finish="isUploading = false"
+        x-on:livewire-upload-error="isUploading = false"
+        x-on:livewire-upload-progress="progress = $event.detail.progress"
+    >
+    <label>Upload File:
+    <input type="file" wire:model="fileToUpload">
+    </label>
+    {{-- <button type="submit">Upload File to {{ $fsdisk }}::{{ $currentLocation }} <div wire:loading wire:target="fileToUpload">...UPLOADING...</div></button> --}}
+    <div x-show="isUploading">
+        <progress max="100" x-bind:value="progress"></progress>
+    </div>
+    </form>
     <table class="border">
         <tr>
+            <th>Media ID</th>
             <th>Thumbnail</th>
             <th>Name</th>
             <th>Size (B)</th>
@@ -30,6 +67,9 @@
         </tr>
         @foreach ($files as $file)
         <tr>
+            <td>
+            {{ $file->id }}
+            </td>
             <td>
                 @if ($file->aggregate_type == 'image')
                 <a href="{{ $file->getUrl() }}">
